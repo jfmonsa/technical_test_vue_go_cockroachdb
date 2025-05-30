4968c9ec968226367edf6d8589914084c72a7c21
jfmonsa <juanmonsalve@protonmail.com>
2025-05-30 10:36:46 -0500

    Add .gitignore and environment variable management for backend

---
+++ backend/app/env_vars.go
@@ -0,0 +1,45 @@
+package env_vars
+
+import "os"
+
+type EnvVars struct {
+	ApiURL    string
+	AuthToken string
+	DBURL     string
+}
+
+// Global variable to hold the initialized environment variables
+// use this instead of calling os.Getenv directly
+var EnvVarsValues = initEnvVars()
+
+// avoid magic strings by defining env variable names (keys) in a struct
+var envVarKeys = struct {
+	externalApiUrl       string
+	externalApiAuthToken string
+	dbUrl                string
+}{
+	externalApiUrl:       "EXTERNAL_API_URL",
+	externalApiAuthToken: "EXTERNAL_API_AUTH_TOKEN",
+	dbUrl:                "DATABASE_URL",
+}
+
+// Initialize environment variables and validate them
+func initEnvVars() EnvVars {
+	envVars := map[string]string{
+		envVarKeys.externalApiUrl:       os.Getenv(envVarKeys.externalApiUrl),
+		envVarKeys.externalApiAuthToken: os.Getenv(envVarKeys.externalApiAuthToken),
+		envVarKeys.dbUrl:                os.Getenv(envVarKeys.dbUrl),
+	}
+
+	// iterate over the environment variables
+	for key, value := range envVars {
+		if value == "" {
+			panic("Environment variable " + key + " is not set")
+		}
+	}
+	return EnvVars{
+		ApiURL:    envVars[envVarKeys.externalApiUrl],
+		AuthToken: envVars[envVarKeys.externalApiAuthToken],
+		DBURL:     envVars[envVarKeys.dbUrl],
+	}
+}
