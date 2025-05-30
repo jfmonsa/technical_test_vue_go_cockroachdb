7e6147c904a0eaae56e372ba0d531234babb098c
jfmonsa <juanmonsalve@protonmail.com>
2025-05-30 15:42:17 -0500

    feat: Add main entry point and database connection setup for stock API

---
+++ backend/src/app/db_conn.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+// GetDBConnection establishes and returns a connection to the PostgreSQL database
+// using the connection string specified in EnvVarsValues.DBURL. If the connection
+// cannot be established, the function logs a fatal error and terminates the application.
+//
+// Parameters:
+//   - ctx: context.Context for managing request-scoped values, cancellation, and timeouts.
+func GetDBConnection(ctx context.Context) *sql.DB {
+	conn, err := sql.Open("postgres", EnvVarsValues.DBURL)
+	if err != nil {
+		log.Fatal("DB Connection Error:", err)
+	}
+	return conn
+}
---
+++ backend/src/main.go
@@ -0,0 +1,37 @@
+// main.go
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/lib/pq" // cockroach driver
+
+	"vue_go_cockroachdb/src/api/stocks"
+	"vue_go_cockroachdb/src/app"
+)
+
+// main is the entry point of the application. It initializes the database connection,
+// sets up the stock repository and HTTP handlers, configures the router with endpoints
+// for retrieving stocks and stock details by ticker, and starts the HTTP server.
+func main() {
+	db := app.GetDBConnection(context.Background())
+
+	repo := stocks.NewCockroachDBStockRepository(db)
+	handler := &stocks.Handler{Repo: repo}
+
+	r := chi.NewRouter()
+
+	// to test:
+	// curl "http://localhost:8080/stocks?page=1&limit=5"
+	r.Get("/stocks", handler.GetStocks)
+
+	// to test:
+	// curl "http://localhost:8080/stocks/AKBA"
+	r.Get("/stocks/{ticker}", handler.GetStockByTicker)
+
+	log.Println("🚀 Server listening ")
+	http.ListenAndServe(":"+app.EnvVarsValues.Port, r)
+}
