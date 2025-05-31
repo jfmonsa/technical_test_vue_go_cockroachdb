f1d088bcea569ec6ddc2b7ef92d504ce7d2e8306
jfmonsa <juanmonsalve@protonmail.com>
2025-05-30 22:38:42 -0500

    feat: add utility function to format dates in Spanish locale

---
+++ frontend/src/utils/dates.ts
@@ -0,0 +1,10 @@
+
+export const formatDate = (dateString: string) => {
+  return new Date(dateString).toLocaleDateString("es-ES", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
