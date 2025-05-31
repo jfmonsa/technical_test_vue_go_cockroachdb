8dd8617d6732bd6e090346d2300e73129a6add33
jfmonsa <juanmonsalve@protonmail.com>
2025-05-30 22:38:56 -0500

    feat: add new icon components for Page, Search, and Stats

---
+++ frontend/src/components/Icons/SearchGreenIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex-shrink-0">
+        <div
+        class="w-8 h-8 bg-green-100 rounded-lg flex items-center justify-center"
+        >
+        <svg
+            class="w-5 h-5 text-green-600"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+        >
+            <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+        </svg>
+        </div>
+    </div>
+</template>
